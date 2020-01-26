c93c94318d39f7bd2ca44f255f42d7ed5767447a
ru4sdevil <54270889+ru4sdevil@users.noreply.github.com>
2020-01-26 14:04:45 +0300

    punct2_1changed

--- script.js
+++ script.js
@@ -4,6 +4,11 @@ punkt2_1()
 function punkt2_1() {
 	//разница в сравнении
 	let d = {key: "value"};
+	let e = {key: "value"} === d;
+	console.log(e);
 	let a = {key: "value"} === d;
 	console.log(a);
+	let b = d;
+	let c = b === d;
+	console.log(c);
 }
